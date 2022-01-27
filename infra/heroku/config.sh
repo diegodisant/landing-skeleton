a47c825e7b9e77ff5ba3421bdcb5bab1a1f21a5e
diegodisant <developerdiego0@gmail.com>
2022-01-26 21:18:30 -0600

    config(env): add APP_HOST

--- infra/heroku/config.sh
+++ infra/heroku/config.sh
@@ -2,6 +2,7 @@
 
 source ../../.env
 
+heroku config:set APP_HOST=${APP_HOST} --app ${HEROKU_APP}
 heroku buildpacks:set heroku-community/static --app ${HEROKU_APP}
 heroku git:remote --app ${HEROKU_APP}
 
