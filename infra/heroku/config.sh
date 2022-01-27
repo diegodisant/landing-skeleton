50e67b2e13a30f0d29759494fe894e93ab9a9825
diegodisant <developerdiego0@gmail.com>
2022-01-26 21:12:26 -0600

    deploy: fix heroku buildpack command

--- infra/heroku/config.sh
+++ infra/heroku/config.sh
@@ -2,7 +2,7 @@
 
 source ../../.env
 
-heroku buildpacks:set heroku-community/static -app ${HEROKU_APP}
+heroku buildpacks:set heroku-community/static --app ${HEROKU_APP}
 heroku git:remote --app ${HEROKU_APP}
 
 exit 0
